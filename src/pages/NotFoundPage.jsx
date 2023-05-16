4ca1a4f7cd4e3f30004559f305801cd16d8d4a8a
Muhammad Talha <talhahanfsd@gmail.com>
2023-05-17 00:31:13 +0500

    Design Not Found Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,10 +1,21 @@
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const NotFoundPage = () => {
   const nav = useNavigate();
   return (
     <div className="app">
-      NOT FOUND <button onClick={() => nav("/")}>GO HOME</button>
+      <Navbar />
+
+      <h1 className="lg:text-[96px] md:text-[69px] text-[48px] font-st text-center pt-32 pb-8">
+        ERROR 404 <span className="text-[#FF7315]">.</span>
+      </h1>
+      <button
+        className="text-[18px] md:text-[24px] lg:text-[30px] font-mr font-black rounded-full px-8 py-4 text-[#232020] bg-[#FF7315] mx-auto block"
+        onClick={() => nav("/")}
+      >
+        GO HOME
+      </button>
     </div>
   );
 };
